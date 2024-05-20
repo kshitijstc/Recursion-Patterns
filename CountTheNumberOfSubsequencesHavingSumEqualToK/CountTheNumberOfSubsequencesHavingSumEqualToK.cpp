85a138f07e75e54d5e6d77ff281bf160b5a839b0
Kshitij M Gajbhiye <144511712+kshitijstc@users.noreply.github.com>
2024-05-20 16:35:31 +0530

    Update CountTheNumberOfSubsequencesHavingSumEqualToK.cpp

--- CountTheNumberOfSubsequencesHavingSumEqualToK/CountTheNumberOfSubsequencesHavingSumEqualToK.cpp
+++ CountTheNumberOfSubsequencesHavingSumEqualToK/CountTheNumberOfSubsequencesHavingSumEqualToK.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+// return type will be int
+int printS(int index,int s,int sum,int arr[],int n){
+    // base case or condition
+    if(s>sum){
+      return 0;
+    }
+    if(index==n){
+        if(s==sum){
+            return 1;
+        }
+        return 0;
+    }
+    // pick
+    s+=arr[index];
+    int l=printS(index+1,s,sum,arr,n);
+
+    // not pick
+    s-=arr[index];
+    int r=printS(index+1,s,sum,arr,n);
+  return l+r;
+}
+
+int main(){
+    int arr[]={1,2,1};
+    int n=3;
+    int sum=2;
+    printS(0,0,sum,arr,n);
+    return 0;
+}
