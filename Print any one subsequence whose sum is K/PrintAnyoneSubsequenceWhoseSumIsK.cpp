6aeced72f564db3924bb1afaeae3f2444b3bcc93
Kshitij M Gajbhiye <144511712+kshitijstc@users.noreply.github.com>
2024-05-20 16:30:29 +0530

    Update PrintAnyoneSubsequenceWhoseSumIsK.cpp

--- Print any one subsequence whose sum is K/PrintAnyoneSubsequenceWhoseSumIsK.cpp
+++ Print any one subsequence whose sum is K/PrintAnyoneSubsequenceWhoseSumIsK.cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+// return type should be bool
+bool printS(int index,vector<int> ds,int s,int sum,int arr[],int n){
+    // base case
+    if(index==n){
+        if(s==sum){
+            for(auto it:ds){cout<<it<<" ";}
+            cout<<endl;
+          return true;
+        }
+        return false;
+    }
+    // pick
+    ds.push_back(arr[index]);
+    s+=arr[index];
+    if(printS(index+1,ds,s,sum,arr,n)==true){
+    return true;
+    }
+
+    // not pick
+    s-=arr[index];
+    ds.pop_back();
+    if(printS(index+1,ds,s,sum,arr,n)==true){
+      return true;
+    }
+  return false;
+}
+
+int main(){
+    int arr[]={1,2,1};
+    int n=3;
+    int sum=2;
+    vector<int> ds;
+    printS(0,ds,0,sum,arr,n);
+    return 0;
+}
